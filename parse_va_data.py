ed96834b149cd9f0693c4b97167d364c96e0d58d
philiphristov <hristovf@cip.ifi.lmu.de>
2019-04-17 16:50:22 +0200

    asdsadas

--- parse_va_data.py
+++ parse_va_data.py
@@ -1,7 +1,7 @@
 import csv
 import re
 import json
-from ast import literal_eval
+import ast
 
 with open('verbaalpina-classifications.csv', newline='') as csv_read:
 	with open('csv_data.csv', 'w', newline='') as csv_writer:
@@ -10,17 +10,29 @@ with open('verbaalpina-classifications.csv', newline='') as csv_read:
 		csv_writer = csv.writer(csv_writer, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 		
 		for row in zooniverse_reader:
-			#parsed_row = [ item.strip('\"') for item in row  ]
-			print(', '.join(row))
-			print("\n")
 
-			parsed_data = "'" + row[11].replace("'", "\"")[1:-1] + "'"
+			parsed_data_values = row[11][1:-1]
+			dict_obj = json.loads(parsed_data_values)
 
-			print(parsed_data)
+			parsed_data_map = row[12]
+			map_data = json.loads(parsed_data_map)
+			map_nr = 0
 
-			# dict_obj = json.loads(parsed_data)
-			dict_obj = literal_eval(parsed_data)
+			for value in map_data:
+				print(map_data[value])
 
-			print(dict_obj)
+				if 'map_id' in map_data[value]:
+					print(map_data[value]["map_id"])
+
+				if 'map_img' in map_data[value]:
+					print(map_data[value]["map_img"])
+
+			
+			
+
+			# values = dict_obj["value"]
+			# print(values)
+			
+
 			print("\n")
 
